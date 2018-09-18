9f32574aaad0214ddd2baae5ce132489f4409a27
Ilja Tkachuk <ilja@heathmont.net>
2018-09-18 12:52:03 +0300

    v0.2.1

--- lib/ex_craft/field.ex
+++ lib/ex_craft/field.ex
@@ -1,4 +1,9 @@
 defmodule ExCraft.Field do
+
+  @doc """
+  Field properties in struct constructor
+  """
+
   @fields [:name, :type, :required, :default, :enforce]
   @enforce_keys @fields
   defstruct     @fields
--- mix.exs
+++ mix.exs
@@ -63,8 +63,8 @@ defmodule ExCraft.MixProject do
       # development tools
       {:excoveralls, "~> 0.8", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 0.5",    only: [:dev, :test], runtime: false},
-      {:ex_doc, "~> 0.18",     only: [:dev, :test], runtime: false},
-      {:credo, "~> 0.8",       only: [:dev, :test], runtime: false},
+      {:ex_doc, "~> 0.19",     only: [:dev, :test], runtime: false},
+      {:credo, "~> 0.9",       only: [:dev, :test], runtime: false},
       {:boilex, "~> 0.2",      only: [:dev, :test], runtime: false},
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
